079efac1519704419a44fb2642dacee3ce9a74e4
domgjntoki <fredsonmenezessumi@gmail.com>
2024-05-27 17:04:41 -0300

    added vpc

---
+++ infra/deploy/network.tf
@@ -0,0 +1,9 @@
+##########################
+# Network Infrastructure #
+##########################
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.1.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
