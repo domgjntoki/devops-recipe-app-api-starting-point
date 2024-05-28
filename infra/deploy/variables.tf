97cb87a3bbd9302b52be6e2a8c707b3890d15081
domgjntoki <fredsonmenezessumi@gmail.com>
2024-05-28 18:43:06 -0300

    added proxy container definition

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -21,3 +21,7 @@ variable "db_username" {
 variable "db_password" {
   description = "Password for the recipe app api database"
 }
+
+variable "ecr_proxy_image" {
+  description = "Path to the ECR repository with the proxy image"
+}
