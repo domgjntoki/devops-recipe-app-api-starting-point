e8b8b3d7cb2f99b8fcc3cd6a4969a2bbc315bd0f
domgjntoki <fredsonmenezessumi@gmail.com>
2024-05-28 18:08:45 -0300

    added ecs cluster

---
+++ infra/deploy/ecs.tf
@@ -0,0 +1,7 @@
+#########################################
+# CS Cluster for running app on Fargate #
+#########################################
+
+resource "aws_ecs_cluster" "main" {
+  name = "${local.prefix}-cluster"
+}
