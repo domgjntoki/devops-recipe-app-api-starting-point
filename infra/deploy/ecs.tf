10ec18c59f79159ce02fc984dc17193fa0fb090b
domgjntoki <fredsonmenezessumi@gmail.com>
2024-05-28 19:27:09 -0300

    fixed port number

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -155,7 +155,7 @@ resource "aws_security_group" "ecs_service" {
   # Outbund access to endpoints
   egress {
     from_port   = 443
-    to_port     = 433
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
