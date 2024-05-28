24c58760c961b481d04e57b766a11b24f98f4228
domgjntoki <fredsonmenezessumi@gmail.com>
2024-05-27 22:53:55 -0300

    changed iam

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -167,6 +167,7 @@ data "aws_iam_policy_document" "rds" {
       "rds:ListTagsForResource",
       "rds:ModifyDBInstance",
       "rds:AddTagsToResource",
+      "rds:ModifyDBSubnetGroup"
     ]
     resources = ["*"]
   }
